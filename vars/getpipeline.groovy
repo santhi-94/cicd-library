2431ecf880a38d756c0b2abad857196d75f7fa2d
santhi.bhogavalli <santhi.bhogavalli@gmail.com>
2020-10-16 09:47:52 +0530

    updated get pipeline

--- vars/getpipeline.groovy
+++ vars/getpipeline.groovy
@@ -1,18 +1,18 @@
 def call(){ 
-def BranchName=branchName()
+	def BranchName=branchName()
 
-if (isFeature(BranchName) || isDev(BranchName)){
-	return 'feature-pipeline.groovy'
-}
-else if (isPR()){
-	return 'pr-pipeline.groovy'	
-}
-else if (isRelease(BranchName) || isMaster(BranchName)){
-	return 'release-pipeline.groovy'
-}
-else{
-	println "please check the branch Naming Convention.. It should be either feature/dev/devlopment/release/master/hotfix "
-}
+	if (isFeature(BranchName) || isDev(BranchName)){
+		return 'feature-pipeline.groovy'
+	}
+	else if (isPR()){
+		return 'pr-pipeline.groovy'	
+	}
+	else if (isRelease(BranchName) || isMaster(BranchName)){
+		return 'release-pipeline.groovy'
+	}
+	else{
+		println "please check the branch Naming Convention.. It should be either feature/dev/devlopment/release/master/hotfix "
+	}
 }
 def branchName(){
 	return env.BRANCH_NAME
