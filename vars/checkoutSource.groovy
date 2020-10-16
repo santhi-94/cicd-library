e8ec423f6a57e5cb9c62f56ed0a04d240eaf21cc
santhi.bhogavalli <santhi.bhogavalli@gmail.com>
2020-10-16 10:01:15 +0530

    updated with code with strings

--- vars/checkoutSource.groovy
+++ vars/checkoutSource.groovy
@@ -1,8 +1,8 @@
 import com.santhi.*
-def  call(String scmurl ,String brancName){
+def  call(String scmurl ,String branchName){
      checkout([$class: 'GitSCM',branches: [[name:"$branchName"]],doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch',localBranch: "**"]],submoduleCfg: [], userRemoteConfigs: [[credentialsId: Constants.github_Creds, url:"$scmurl"]]])	
 }
-def  call(String scmurl,String dir ,String brancName){
+def  call(String scmurl,String dir ,String branchName){
      checkout([$class: 'GitSCM',branches: [[name:"$branchName"]],doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'LocalBranch',localBranch: "**"]],extensions: [[$class: 'RelativeTargetDirectory', 
             relativeTargetDir: "dir"]],submoduleCfg: [], userRemoteConfigs: [[credentialsId: Constants.github_Creds, url:"$scmurl"]]])	
 }
