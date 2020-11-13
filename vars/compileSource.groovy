73090ece22d4eb974ffb002ebab4075d58298b84
santhi.bhogavalli <santhi.bhogavalli@gmail.com>
2020-11-13 08:02:01 +0530

    updated with bat commands

--- vars/compileSource.groovy
+++ vars/compileSource.groovy
@@ -1,6 +1,9 @@
 import com.santhi.*
 def call(String mvngoals,String mvnparameters){
-	sh """
-		mvn $mvngoals $mvnparameters # (goals - clean,install,deploy,release:prepare,release:perform)(parameters- -Darguments="-Dmaven.test.skip=false,-Dmaven.test.failure.ignore=true")
-	"""
+//	sh """
+//		mvn $mvngoals $mvnparameters # (goals - clean,install,deploy,release:prepare,release:perform)(parameters- -Darguments="-Dmaven.test.skip=false,-Dmaven.test.failure.ignore=true")
+//	"""
+       def mvnHome = tool 'Maven'
+               bat "\"${mvnHome}\"\\bin\\mvn --version"
+               bat "\"${mvnHome}\"\\bin\\mvn $mvngoals $mvnparameters"
 }
